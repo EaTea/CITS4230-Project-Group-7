4baa6f6dcbbda4ec3564c6be8a3da967cda156a6
S0uten <himespike@gmail.com>
2011-08-29 21:23:04 +0800

    FAQ page: Added the FAQ page from EaTea's branch with some changes made

---
+++ WWW/faq/contents.php
@@ -0,0 +1,35 @@
+		<h1>Frequently Asked Questions</h1>
+		<p>
+			Got questions on about XYZ? Here are some frequently asked questions that might help.
+		</p>
+		<ul>
+			<li>
+				<h3>How do I add a new To-Do List to my lists?</h3>
+				<p>
+					If you want to add a new To-Do List, you should go to your dashboard, where you can see all your lists. The dashboard should display all your current to-do lists and in the top right-hand corner you should see a "add new To-Do List". Click that, and follow the steps to create a new To-Do List.
+				</p>
+			</li>
+			<li>
+				<h3>I want to share one of my lists with a friend. How can I do this?</h3>
+				<p>
+					If you have a list you want to share with a friend, then all you need to dois go to your dashboard, click on the list you want to share, select "Edit List" and, on the next screen press the "Add Friends" button. Simply search for the name or username of the friend you want to share the list with, click on the resulting user you want to share with, and then click "Confirm".
+				</p>
+			</li>
+			<li>
+				<h3>I've shared a list with a friend, and they can view the list, but I want to let them add and remove tasks too.</h3>
+				<p>
+					You've shared a list, but your friend can't do everything that they want to. This is because your friend hasn't been given permission to take these actions &#8211; you need to go to your dashboard, select the list you've shared, and click to edit it.
+				</p>
+				<p>
+					You should be able to see a list of people who you've shared the list with. You can then allow or disallow someone from doing certain things (such as adding or removing tasks) by ticking or unticking the boxes next to their names.
+				</p>
+			</li>
+			<li>
+				<h3>Another question.</h3>
+			</li>
+		</ul>
+		<!--<aside>-->
+		<p>
+			Still confused? Never fear! Visit our <a href="<?php echo $top_level; ?>/help">help section</a> to learn more.
+		</p>
+		<!--</aside>-->
