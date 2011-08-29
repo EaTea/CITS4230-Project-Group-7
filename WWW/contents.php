d509ec9895abebd7d2000395012dc49b20e7fd4f
sontobgay <sontobdrukpa@gmail.com>
2011-08-29 10:13:28 +0800

    Home: Added registration forms in contents.php

--- WWW/contents.php
+++ WWW/contents.php
@@ -1,8 +1,61 @@
-		<!-- home banner -->
-		<div id="banner">
-			<a href="<?php echo $top_level; ?>">
-				<img src="images/banner.jpg" alt="XYZ: The Shared Todo List" title="XYZ: The Shared Todo List" />
-			</a>
-		</div>
-		<h1>XYZ</h1>
-		<p>This is a shared todo site.</p>
+        <article id="ex_main">
+        <header>
+        <h3>Shared TO-DO List lets you to connect and share Lists with your Friends!</h3>
+        </header>
+        <div class="front_image">
+        </div>
+        <footer>
+        <p><a href="#">Learn more...</a></p>
+        </footer>
+        </article>
+
+        <aside id="registration">
+        <div id="reg_form_container">
+        <h3>Sign Up to Share List:</h3>
+        <form id="reg_form">
+            <div id="reg_form_box" class="general_form">
+             <table class="inputs" cellspacing="0" cellpadding="1">
+               <tbody>
+                <tr>
+                   <td class="label"><label for="first_name">First Name:</label></td>
+                   <td><div class="field_container"><input id="first_name" name="first_name" type="text" placeholder="Your Given Names"></div></td>
+                </tr>
+                <tr>
+                   <td class="label"><label for="last_name">Last Name:</label></td>
+                   <td><div class="field_container"><input id="last_name" name="last_name" type="text" placeholder="Your Surname"/></div></td>
+                </tr>
+                <tr>
+                   <td class="label"><label for="email">Email address:</label></td> 
+                   <td><div class="field_container"><input id="email" name="email" type="email"/></div></td>
+                </tr>
+                <tr>
+                   <td class="label"><label for="email_confirm">Confirm Email:</label></td> 
+                   <td><div class="field_container"><input id="email_confirm" name="email_confirm" type="email"/></div></td>
+                </tr>
+                <tr>
+                   <td class="passwd"><label for="passpwd">New Password:</label></td> 
+                   <td><div class="field_container"><input id="passwd" name="passwd" type="password" required /></div></td>
+                </tr>
+                <tr>
+                    <td class="label">Gender:</td>
+                    <td><div class="field_container">
+                          <select id="gender" class="select" name="gender">
+                            <option value="0">Select Sex:</option>
+                            <option value="1">Female</option>
+                            <option value="2">Male</option>
+                          </select>
+                        </div>
+                    </td>
+                </tr>
+               </tbody>
+             </table>
+             <div class="reg_button">
+                 <input id="sign_up" type="submit" value="Register"/>
+             </div>
+           </div>
+        </form>
+        </div>
+        </aside>
+
+        <!-- make the middle region's background color expand -->
+        <div class="clear"></div>
--- WWW/global/footer.php
+++ WWW/global/footer.php
@@ -2,6 +2,12 @@
 	<!-- footer -->
 	<footer>
 		<nav id="bottom_bar">
+                    <ul>
+                      <li><a href="#">Link1</a></li>
+                      <li><a href="#">Link2</a></li>
+                      <li><a href="#">Link3</a></li>
+                      <li><a href="#">Link4</a></li>
+                    </ul>
 		</nav>
 	</footer>
 </body>
