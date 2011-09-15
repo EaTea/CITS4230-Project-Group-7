e8b0ec6e609050fb1d39aa710bdeb7fb1becc14e
S0uten <himespike@gmail.com>
2011-09-16 01:45:49 +0800

    Footer: Removed the W3C-validated logos

--- WWW/global/footer.php
+++ WWW/global/footer.php
@@ -17,10 +17,10 @@
 		<div id="validators">
 			<ul>
 				<li>
-					<a href="http://validator.w3.org/check?uri=referer"><img style="border:0;width:88px;height:31px" src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0!" /></a>
+					<a href="http://validator.w3.org/check?uri=referer">Valid (X)HTML5</a>
 				</li>
 				<li>
-					<a href="http://jigsaw.w3.org/css-validator/check/referer"><img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" /></a>
+					<a href="http://jigsaw.w3.org/css-validator/check/referer">Valid CSS3</a>
 				</li>
 			</ul>
 		</div>
