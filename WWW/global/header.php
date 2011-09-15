ba0a3e06b7abecb1efaa2dbb6127fbcee9bbb8fa
S0uten <himespike@gmail.com>
2011-09-16 01:31:48 +0800

    Header: Modified the markup structure

--- WWW/global/header.php
+++ WWW/global/header.php
@@ -12,27 +12,27 @@
 	<script type="application/ecmascript" src="script.es"></script>
 </head>
 <body>
-	<!-- accessibility access -->
-	<nav hidden="hidden" id="accessibility_access">
-		<a href="#main" accesskey="c">Go to page content</a>
-		<a href="#top_bar" accesskey="m">Go to main menu</a>
-		<a href="#bottom_bar" accesskey="i">Go to content info</a>
-		<!--<a href="#s" accesskey="s" onclick="$('#s').focus(); return false;">Go to the search field</a>-->
-	</nav>
 	<!-- header -->
 	<header>
+		<!-- accessibility access -->
+		<nav hidden="hidden" id="accessibility_access">
+			<a href="#content" accesskey="c">Go to page content</a>
+			<a href="#top_bar" accesskey="m">Go to main menu</a>
+			<a href="#bottom_bar" accesskey="i">Go to content info</a>
+			<!--<a href="#s" accesskey="s" onclick="$('#s').focus(); return false;">Go to the search field</a>-->
+		</nav>
 		<!-- domain bar -->
 		<div id="domain_bar">
 			<div>
 				<a href="<?php echo $top_level; ?>"><strong><?php echo $top_host; ?></strong><?php echo ucwords(str_replace("_"," ", str_replace("/", " › ", $current_path))); ?></a>
 			</div>
 		</div>
-		<!-- top navbar -->
-		<nav id="top_bar">
-			<ul>
-				<li><a href="<?php echo $top_level; ?>" title="Home">Home</a></li>
-			</ul>
-		</nav>
 	</header>
-	<!-- main contents -->
-	<section id="main">
+	<!-- top navbar -->
+	<nav id="top_bar">
+		<ul>
+			<li><a href="<?php echo $top_level; ?>" title="Home">Home</a></li>
+		</ul>
+	</nav>
+	<!-- page content -->
+	<section id="content">
