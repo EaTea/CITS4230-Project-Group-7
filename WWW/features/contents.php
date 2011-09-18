74bd569c3529715b2e86b8bb53605578c9dd6525
EaTea <ed.r.tay@gmail.com>
2011-09-19 01:10:35 +0800

    Features page: Add filler text based upon Stage 2 Specifications

---
+++ WWW/features/contents.php
@@ -0,0 +1,21 @@
+		<!-- Features -->
+		<h1>Features</h1>
+		I guess you're wondering what kind of things you'll be able to do on XYZ? Plenty! Take a look below at some of XYZ's features:
+		<ul>
+			<li>
+				Customisable tasks, from their creation to their completion and details
+			</li>
+			<li>
+				Categorise your tasks: add a task to a list (or lists!) of things-to-do and then share it with your friends and colleagues
+			</li>
+			<li>
+				List permissions: need to make a list that people can view, but they can't change? It's easy with XYZ's List Permissions system
+			</li>
+			<li>
+				Automated Email Reminders: yep, we too are looking forward to flooding your emails with more spam (but we really hope that you use our website instead)
+			</li>
+			<li>
+				Offline Storage: Can't actually get to XYZ at a point in time? Never fear! You can use our handy Local Synch feature to get a copy of your To-Do List and store it on your computer
+			</li>
+		</ul>
+		If you've got some questions, you should go to our <a href="<?php echo $top_level;?>/faq"/>FAQ</a> to learn more.
