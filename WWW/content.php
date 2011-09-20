6aed3066b9cd735da7074b4e2b238812f6b6829b
S0uten <himespike@gmail.com>
2011-09-21 00:22:14 +0800

    Home page: Clean, simplify, and fix margin issues

--- WWW/content.php
+++ WWW/content.php
@@ -1,4 +1,4 @@
-		<div id="main_content">
+		<div id="content">
 			<header>
 				<h1>Shared TO-DO List lets you to connect and share Lists with your Friends!</h1>
 			</header>
@@ -20,38 +20,35 @@
 		</div>
 
 		<aside id="registration">
-			<div id="reg_form" class="todo-form">
-				<form id="form" name="form" method="post" action="#">
-					<h1>To-Do List Sign Up</h1>
-					<p>Register and share list with your friends!</p>
+			<form id="signup_form" name="signup" method="post" action="#">
+				<h1>To-Do List Sign Up</h1>
+				<p>Register and share list with your friends!</p>
 
-					<label>Name
-						<span class="highlight">Enter your name</span>
-					</label>
-					<input type="text" name="name" id="name" required="required" />
+				<label>Name
+					<span class="highlight">Enter your name</span>
+				</label>
+				<input type="text" name="name" id="name" required="required" />
 
-					<label>Email
-						<span class="highlight">Enter an email address</span>
-					</label>
-					<input type="email" name="email" id="email" required="required" />
+				<label>Email
+					<span class="highlight">Enter an email address</span>
+				</label>
+				<input type="email" name="email" id="email" required="required" />
 
-					<label>Confirm Email
-						<span class="highlight">Confirm the above email</span>
-					</label>
-					<input type="email" name="confirm_email" id="confirm_email" required="required" />
+				<label>Confirm Email
+					<span class="highlight">Confirm the above email</span>
+				</label>
+				<input type="email" name="confirm_email" id="confirm_email" required="required" />
 
-					<label>Password
-						<span class="highlight">Min. size 6 chars</span>
-					</label>
-					<input type="password" name="password" id="password" required="required" />
+				<label>Password
+					<span class="highlight">Min. size 6 chars</span>
+				</label>
+				<input type="password" name="password" id="password" required="required" />
 
-					<label>Confirm Password
-						<span class="highlight">Min. size 6 chars</span>
-					</label>
-					<input type="password" name="confirm_password" id="confirm_password" required="required" />
+				<label>Confirm Password
+					<span class="highlight">Min. size 6 chars</span>
+				</label>
+				<input type="password" name="confirm_password" id="confirm_password" required="required" />
 
-					<button type="submit">Sign Up</button>
-					<div class="spacer"></div>
-				</form>
-			</div>
+				<button type="submit">Sign Up</button>
+			</form>
 		</aside>
