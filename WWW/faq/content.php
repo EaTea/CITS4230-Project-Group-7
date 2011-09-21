16d5496fd254b0ae7609f5528e4b57a548c41e38
S0uten <himespike@gmail.com>
2011-09-21 20:51:42 +0800

    FAQ page: Edit text (GH-8)

--- WWW/faq/content.php
+++ WWW/faq/content.php
@@ -16,25 +16,25 @@
 			<section>
 				<h2>But then why is this website so awesome? It only has 3 people working on it!</h2>
 				<p>
-					Yuki and Sonam have tiger's blood running in their veins. Can't help it if we're bi-winning. Now, read the rest of the FAQ 'cause I'm sure that you cam here with a serious question.
+					Yuki and Sonam have tiger's blood running in their veins. Can't help it if we're bi-winning. Now, read the rest of the FAQ 'cause I'm sure that you came here with a serious question.
 				</p>
 			</section>
 			<section>
-				<h2>There's another product called "XYZ" already! Aren't you copying them a little bit?</h2>
+				<h2>There's another product called "XYZ" already! Aren't you kind of copying them?</h2>
 				<p>
 					We didn't even know they existed, up until one of us (Edwin) was informed by a friend. And they don't have Menma's blessing. So to be technical, they're copying us.
 				</p>
 			</section>
 			<section>
-				<h2>How do I add a new To-Do List to my lists?</h2>
+				<h2>How do I add a new To-do List to my lists?</h2>
 				<p>
-					If you want to add a new To-Do List, you should go to your dashboard, where you can see all your lists. The dashboard should display all your current to-do lists and in the top right-hand corner you should see a "add new To-Do List". Click that, and follow the steps to create a new To-Do List.
+					If you want to add a new To-do List, you should go to your dashboard, where you can see all your lists. The dashboard should display all your current To-do Lists and in the top right-hand corner you should see a "add new To-do List". Click that, and follow the steps to create a new To-do List.
 				</p>
 			</section>
 			<section>
 				<h2>I want to share one of my lists with a friend. How can I do this?</h2>
 				<p>
-					If you have a list you want to share with a friend, then all you need to dois go to your dashboard, click on the list you want to share, select "Edit List" and, on the next screen press the "Add Friends" button. Simply search for the name or username of the friend you want to share the list with, click on the resulting user you want to share with, and then click "Confirm".
+					If you have a list you want to share with a friend, then all you need to do is go to your dashboard, click on the list you want to share, select "Edit List", and on the next screen, press the "Add Friends" button. Simply search for the name or username of the friend you want to share the list with, click on the target user, and then click "Confirm".
 				</p>
 			</section>
 			<section>
@@ -46,13 +46,13 @@
 			<section>
 				<h2>How do I mark something as completed or remove it from my tasks?</h2>
 				<p>
-					It's pretty simple. If you go to your task viewer, you'll notice that there's a little drop-down icon. If you click on it, you'll bring up a little menu, and you can choose to remove, or mark something as completed there.
+					It's pretty simple. If you go to your task viewer, you'll notice that there's a little drop-down icon. Click on it, and it'll bring up a little menu where you can choose to remove or mark a task as completed.
 				</p>
 			</section>
 			<section>
-				<h2>I want to store my To-Do List offline. How do I use the offline synch?</h2>
+				<h2>I want to store my To-do List offline. How do I use the offline synch?</h2>
 				<p>
-					All you have to do is go to your list, select "Edit List", and in the corresponding panel, you can select "Export to My Computer". You'll be asked where you want to save the offline copy, and then hey! presto! Your To-Do list will be stored offline for you.
+					All you have to do is go to your list, select "Edit List", and in the corresponding panel, you can select "Export to Local Computer". You'll be asked where you want to save the offline copy, and then, presto! Your To-do List will be saved for offline use.
 				</p>
 			</section>
 			<section>
@@ -70,11 +70,16 @@
 			<section>
 				<h2>Really? Upgrade my browser? I guess I could...</h2>
 				<p>
-					Or you could support these wonderful open-source projects, download one of these browsers (for free) and try them out:
+					Or you could support these wonderful open-source projects, and download (for free) one of these web browsers:
 				</p>
 				<ul>
-					<li><a href="http://www.mozilla.org/en-US/firefox/">Mozilla Firefox</a></li>
+					<li><a href="http://www.firefox.com/">Mozilla Firefox</a></li>
 					<li><a href="http://www.google.com/chrome">Google Chrome</a></li>
+				</ul>
+				<p>
+					Or this excellent and free, but proprietary, web browser:
+				</p>
+				<ul>
 					<li><a href="http://www.opera.com/">Opera</a></li>
 				</ul>
 			</section>
@@ -82,6 +87,6 @@
 		</article>
 		<!--<aside>
 			<p>
-				Still confused? Never fear! Visit our <a href="<?php echo $top_level; ?>/help">help section</a> to learn more.
+				Still confused? Fear not! Visit our <a href="<?php echo $top_level; ?>/help">help section</a> to learn more.
 			</p>
 		</aside>-->
