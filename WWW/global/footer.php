627b9687f07fe5a176384b554d8c2ec948c36c02
S0uten <himespike@gmail.com>
2011-09-21 22:22:21 +0800

    Footer: Extend bottom bar, and improve readability (GH-2)

--- WWW/global/footer.php
+++ WWW/global/footer.php
@@ -1,6 +1,7 @@
 	</section>
 	<!-- footer -->
 	<footer>
+		<div>
 		<nav id="bottom_bar">
 			<script type='application/ecmascript'>var a = new Array('.edu','@stu','2050','3332','dent','.uwa','.au');document.write(" : Maintained by <a href='&#109;&#97;&#105;&#108;&#116;&#111;&#58;"+a[2]+a[3]+a[1]+a[4]+a[5]+a[0]+a[6]+"'>CITS4230 Group 7</a>");</script>
 			<ul>
@@ -18,6 +19,8 @@
 				<li><a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3&amp;vextwarning=true">Valid CSS3</a></li>
 			</ul>
 		</aside>
+		</div>
+		<div></div>
 	</footer>
 </body>
 </html>
