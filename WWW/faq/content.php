6fa75db3ea5bc07d22fe574a1d321033cb218351
S0uten <himespike@gmail.com>
2011-09-21 20:19:34 +0800

    FAQ page: Improve semantic meaning of the markup (GH-8)

--- WWW/faq/content.php
+++ WWW/faq/content.php
@@ -1,86 +1,87 @@
 		<!-- FAQ Subject to change as Stage II implemented -->
-		<h1>Frequently Asked Questions</h1>
-		<p>
-			Got questions on about XYZ? Here are some frequently asked questions that might help.
-		</p>
-		<ul>
-			<li>
-				<h3>Who worked on this website?</h3>
+		<article id="faq">
+			<div class="doc">
+			<h1>Frequently Asked Questions</h1>
+			<p>
+				Got questions on about XYZ? Here are some frequently asked questions that might help.
+			</p>
+			</div>
+			<div id="faq_list">
+			<section>
+				<h2>Who worked on this website?</h2>
 				<p>
 					Yuki Osada (20503332) and Sonam Tobgay (20976539). And Edwin Tay (20529864) did something too.
 				</p>
-			</li>
-			<li>
-				<h3>But then why is this website so awesome? It only has 3 people working on it!</h3>
+			</section>
+			<section>
+				<h2>But then why is this website so awesome? It only has 3 people working on it!</h2>
 				<p>
 					Yuki and Sonam have tiger's blood running in their veins. Can't help it if we're bi-winning. Now, read the rest of the FAQ 'cause I'm sure that you cam here with a serious question.
 				</p>
-			</li>
-			<li>
-				<h3>There's another product called "XYZ" already! Aren't you copying them a little bit?</h3>
+			</section>
+			<section>
+				<h2>There's another product called "XYZ" already! Aren't you copying them a little bit?</h2>
 				<p>
 					We didn't even know they existed, up until one of us (Edwin) was informed by a friend. And they don't have Menma's blessing. So to be technical, they're copying us.
 				</p>
-			</li>
-			<li>
-				<h3>How do I add a new To-Do List to my lists?</h3>
+			</section>
+			<section>
+				<h2>How do I add a new To-Do List to my lists?</h2>
 				<p>
 					If you want to add a new To-Do List, you should go to your dashboard, where you can see all your lists. The dashboard should display all your current to-do lists and in the top right-hand corner you should see a "add new To-Do List". Click that, and follow the steps to create a new To-Do List.
 				</p>
-			</li>
-			<li>
-				<h3>I want to share one of my lists with a friend. How can I do this?</h3>
+			</section>
+			<section>
+				<h2>I want to share one of my lists with a friend. How can I do this?</h2>
 				<p>
 					If you have a list you want to share with a friend, then all you need to dois go to your dashboard, click on the list you want to share, select "Edit List" and, on the next screen press the "Add Friends" button. Simply search for the name or username of the friend you want to share the list with, click on the resulting user you want to share with, and then click "Confirm".
 				</p>
-			</li>
-			<li>
-				<h3>I've shared a list with a friend, and they can view the list, but I want to let them add and remove tasks too.</h3>
+			</section>
+			<section>
+				<h2>I've shared a list with a friend, and they can view the list, but I want to let them add and remove tasks too.</h2>
 				<p>
-					You've shared a list, but your friend can't do everything that they want to. This is because your friend hasn't been given permission to take these actions &#8211; you need to go to your dashboard, select the list you've shared, and click to edit it.
+					You've shared a list, but your friend can't do everything that they want to. This is because your friend hasn't been given permission to take these actions &#8211; you need to go to your dashboard, select the list you've shared, and click to edit it. You should be able to see a list of people who you've shared the list with. You can then allow or disallow someone from doing certain things (such as adding or removing tasks) by ticking or unticking the boxes next to their names.
 				</p>
-				<p>
-					You should be able to see a list of people who you've shared the list with. You can then allow or disallow someone from doing certain things (such as adding or removing tasks) by ticking or unticking the boxes next to their names.
-				</p>
-			</li>
-			<li>
-				<h3>How do I mark something as completed or remove it from my tasks?</h3>
+			</section>
+			<section>
+				<h2>How do I mark something as completed or remove it from my tasks?</h2>
 				<p>
 					It's pretty simple. If you go to your task viewer, you'll notice that there's a little drop-down icon. If you click on it, you'll bring up a little menu, and you can choose to remove, or mark something as completed there.
 				</p>
-			</li>
-			<li>
-				<h3>I want to store my To-Do List offline. How do I use the offline synch?</h3>
+			</section>
+			<section>
+				<h2>I want to store my To-Do List offline. How do I use the offline synch?</h2>
 				<p>
 					All you have to do is go to your list, select "Edit List", and in the corresponding panel, you can select "Export to My Computer". You'll be asked where you want to save the offline copy, and then hey! presto! Your To-Do list will be stored offline for you.
 				</p>
-			</li>
-			<li>
-				<h3>This website isn't working on my browser!</h3>
+			</section>
+			<section>
+				<h2>This website isn't working on my browser!</h2>
 				<p>
 					Are you using Internet Explorer with a version earlier than 8?
 				</p>
-			</li>
-			<li>
-				<h3>Why, yes I am! How did you guess?</h3>
+			</section>
+			<section>
+				<h2>Why, yes I am! How did you guess?</h2>
 				<p>
 					We're just awesome like that. Tell you what, why don't you go and upgrade your browser? It won't cost you a cent.
 				</p>
-			</li>
-			<li>
-				<h3>Really? Upgrade my browser? I guess I could...</h3>
+			</section>
+			<section>
+				<h2>Really? Upgrade my browser? I guess I could...</h2>
 				<p>
 					Or you could support these wonderful open-source projects, download one of these browsers (for free) and try them out:
-					<ul>
-						<li><a href="http://www.mozilla.org/en-US/firefox/">Mozilla Firefox</a></li>
-						<li><a href="http://www.google.com/chrome">Google Chrome</a></li>
-						<li><a href="http://www.opera.com/">Opera</a></li>
-					</ul>
 				</p>
-			</li>
-		</ul>
+				<ul>
+					<li><a href="http://www.mozilla.org/en-US/firefox/">Mozilla Firefox</a></li>
+					<li><a href="http://www.google.com/chrome">Google Chrome</a></li>
+					<li><a href="http://www.opera.com/">Opera</a></li>
+				</ul>
+			</section>
+			</div>
+		</article>
 		<!--<aside>
-		<p>
-			Still confused? Never fear! Visit our <a href="<?php echo $top_level; ?>/help">help section</a> to learn more.
-		</p>
+			<p>
+				Still confused? Never fear! Visit our <a href="<?php echo $top_level; ?>/help">help section</a> to learn more.
+			</p>
 		</aside>-->
