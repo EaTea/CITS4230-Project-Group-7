4c6530615906b1aeaf3692c379864cb7a0da35cf
S0uten <himespike@gmail.com>
2011-09-23 05:37:47 +0800

    Formatting: Apply consistent indenting

--- WWW/faq/content.php
+++ WWW/faq/content.php
@@ -1,88 +1,88 @@
 		<!-- FAQ Subject to change as Stage II implemented -->
 		<article id="faq">
 			<div class="doc">
-			<h1>Frequently Asked Questions</h1>
-			<p>
-				Got questions on about FMN? Here are some frequently asked questions that might help.
-			</p>
+				<h1>Frequently Asked Questions</h1>
+				<p>
+					Got questions on about FMN? Here are some frequently asked questions that might help.
+				</p>
 			</div>
 			<div class="tlist">
-			<section>
-				<h2>Who worked on this website?</h2>
-				<p>
-					Yuki Osada (20503332) and Sonam Tobgay (20976539). And Edwin Tay (20529864) did something too.
-				</p>
-			</section>
-			<section>
-				<h2>But then why is this website so awesome? It only has 3 people working on it!</h2>
-				<p>
-					Yuki and Sonam have tiger's blood running in their veins. Can't help it if we're bi-winning. Now, read the rest of the FAQ 'cause I'm sure that you came here with a serious question.
-				</p>
-			</section>
-			<section>
-				<h2>There's another product called "FMN" already! Aren't you kind of copying them?</h2>
-				<p>
-					We didn't even know they existed, up until one of us (Edwin) was informed by a friend. And they don't have Menma's blessing. So to be technical, they're copying us.
-				</p>
-			</section>
-			<section>
-				<h2>How do I add a new to-do list to my lists?</h2>
-				<p>
-					If you want to add a new to-do list, you should go to your dashboard, where you can see all your lists. The dashboard should display all your current to-do lists and in the top right-hand corner you should see a "add new to-do list". Click that, and follow the steps to create a new to-do list.
-				</p>
-			</section>
-			<section>
-				<h2>I want to share one of my lists with a friend. How can I do this?</h2>
-				<p>
-					If you have a list you want to share with a friend, then all you need to do is go to your dashboard, click on the list you want to share, select "Edit List", and on the next screen, press the "Add Friends" button. Simply search for the name or username of the friend you want to share the list with, click on the target user, and then click "Confirm".
-				</p>
-			</section>
-			<section>
-				<h2>I've shared a list with a friend, and they can view the list, but I want to let them add and remove tasks too.</h2>
-				<p>
-					You've shared a list, but your friend can't do everything that they want to. This is because your friend hasn't been given permission to take these actions &#8211; you need to go to your dashboard, select the list you've shared, and click to edit it. You should be able to see a list of people who you've shared the list with. You can then allow or disallow someone from doing certain things (such as adding or removing tasks) by ticking or unticking the boxes next to their names.
-				</p>
-			</section>
-			<section>
-				<h2>How do I mark something as completed or remove it from my tasks?</h2>
-				<p>
-					It's pretty simple. If you go to your task viewer, you'll notice that there's a little drop-down icon. Click on it, and it'll bring up a little menu where you can choose to remove or mark a task as completed.
-				</p>
-			</section>
-			<section>
-				<h2>I want to store my to-do list offline. How do I use the offline synch?</h2>
-				<p>
-					All you have to do is go to your list, select "Edit List", and in the corresponding panel, you can select "Export to Local Computer". You'll be asked where you want to save the offline copy, and then, presto! Your to-do list will be saved for offline use.
-				</p>
-			</section>
-			<section>
-				<h2>This website isn't working on my browser!</h2>
-				<p>
-					Are you using Internet Explorer with a version earlier than 8?
-				</p>
-			</section>
-			<section>
-				<h2>Why, yes I am! How did you guess?</h2>
-				<p>
-					We're just awesome like that. Tell you what, why don't you go and upgrade your browser? It won't cost you a cent.
-				</p>
-			</section>
-			<section>
-				<h2>Really? Upgrade my browser? I guess I could...</h2>
-				<p>
-					Or you could support those wonderful open-source projects, and download (for free) one of their web browsers:
-				</p>
-				<ul>
-					<li><a href="http://www.firefox.com/">Mozilla Firefox</a></li>
-					<li><a href="http://www.google.com/chrome">Google Chrome</a></li>
-				</ul>
-				<p>
-					Or even an excellent and free, but proprietary, web browser:
-				</p>
-				<ul>
-					<li><a href="http://www.opera.com/">Opera</a></li>
-				</ul>
-			</section>
+				<section>
+					<h2>Who worked on this website?</h2>
+					<p>
+						Yuki Osada (20503332) and Sonam Tobgay (20976539). And Edwin Tay (20529864) did something too.
+					</p>
+				</section>
+				<section>
+					<h2>But then why is this website so awesome? It only has 3 people working on it!</h2>
+					<p>
+						Yuki and Sonam have tiger's blood running in their veins. Can't help it if we're bi-winning. Now, read the rest of the FAQ 'cause I'm sure that you came here with a serious question.
+					</p>
+				</section>
+				<section>
+					<h2>There's another product called "FMN" already! Aren't you kind of copying them?</h2>
+					<p>
+						We didn't even know they existed, up until one of us (Edwin) was informed by a friend. And they don't have Menma's blessing. So to be technical, they're copying us.
+					</p>
+				</section>
+				<section>
+					<h2>How do I add a new to-do list to my lists?</h2>
+					<p>
+						If you want to add a new to-do list, you should go to your dashboard, where you can see all your lists. The dashboard should display all your current to-do lists and in the top right-hand corner you should see a "add new to-do list". Click that, and follow the steps to create a new to-do list.
+					</p>
+				</section>
+				<section>
+					<h2>I want to share one of my lists with a friend. How can I do this?</h2>
+					<p>
+						If you have a list you want to share with a friend, then all you need to do is go to your dashboard, click on the list you want to share, select "Edit List", and on the next screen, press the "Add Friends" button. Simply search for the name or username of the friend you want to share the list with, click on the target user, and then click "Confirm".
+					</p>
+				</section>
+				<section>
+					<h2>I've shared a list with a friend, and they can view the list, but I want to let them add and remove tasks too.</h2>
+					<p>
+						You've shared a list, but your friend can't do everything that they want to. This is because your friend hasn't been given permission to take these actions &#8211; you need to go to your dashboard, select the list you've shared, and click to edit it. You should be able to see a list of people who you've shared the list with. You can then allow or disallow someone from doing certain things (such as adding or removing tasks) by ticking or unticking the boxes next to their names.
+					</p>
+				</section>
+				<section>
+					<h2>How do I mark something as completed or remove it from my tasks?</h2>
+					<p>
+						It's pretty simple. If you go to your task viewer, you'll notice that there's a little drop-down icon. Click on it, and it'll bring up a little menu where you can choose to remove or mark a task as completed.
+					</p>
+				</section>
+				<section>
+					<h2>I want to store my to-do list offline. How do I use the offline synch?</h2>
+					<p>
+						All you have to do is go to your list, select "Edit List", and in the corresponding panel, you can select "Export to Local Computer". You'll be asked where you want to save the offline copy, and then, presto! Your to-do list will be saved for offline use.
+					</p>
+				</section>
+				<section>
+					<h2>This website isn't working on my browser!</h2>
+					<p>
+						Are you using Internet Explorer with a version earlier than 8?
+					</p>
+				</section>
+				<section>
+					<h2>Why, yes I am! How did you guess?</h2>
+					<p>
+						We're just awesome like that. Tell you what, why don't you go and upgrade your browser? It won't cost you a cent.
+					</p>
+				</section>
+				<section>
+					<h2>Really? Upgrade my browser? I guess I could...</h2>
+					<p>
+						Or you could support those wonderful open-source projects, and download (for free) one of their web browsers:
+					</p>
+					<ul>
+						<li><a href="http://www.firefox.com/">Mozilla Firefox</a></li>
+						<li><a href="http://www.google.com/chrome">Google Chrome</a></li>
+					</ul>
+					<p>
+						Or even an excellent and free, but proprietary, web browser:
+					</p>
+					<ul>
+						<li><a href="http://www.opera.com/">Opera</a></li>
+					</ul>
+				</section>
 			</div>
 		</article>
 		<!--<aside>
