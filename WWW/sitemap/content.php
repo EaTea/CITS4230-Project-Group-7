606c7a5164c9aa76a106f7fc2fb8f56a52d728b5
EaTea <ed.r.tay@gmail.com>
2011-09-23 03:05:20 +0800

    SiteMap: Styled page and added list styling for these specific elements using CSS classes.

--- WWW/sitemap/content.php
+++ WWW/sitemap/content.php
@@ -1,30 +1,30 @@
-		<h1>Site Map</h1>
-		<ul>
+		<h1 class="pageTitle">Site Map</h1>
+		<ul class="styledList">
 			<li>
-				<h3>
-					<a title="Home Page" href="<?php echo $rel_path;?>">
+				<h3 class="itemTitle">
+					<a title="Home Page" href="<?php echo $rel_path;?>" class="genLink">
 						Home
 					</a>
 				</h3>
 			</li>
-			<ul>
+			<ul class="styledList">
 				<li>
-					<h3>
-						<a title="FAQ" href="<?php echo $rel_path;?>faq">
+					<h3 class="itemTitle">
+						<a title="FAQ" href="<?php echo $rel_path;?>faq" class="genLink">
 							FAQ
 						</a>
 					</h3>
 				</li>
 				<li>
-					<h3>
-						<a title="Features" href="<?php echo $rel_path;?>features">
+					<h3 class="itemTitle">
+						<a title="Features" href="<?php echo $rel_path;?>features" class="genLink">
 							Features
 						</a>
 					</h3>
 				</li>
 				<li>
-					<h3>
-						<a title="Privacy Policy" href="<?php echo $rel_path;?>privacy">
+					<h3 class="itemTitle">
+						<a title="Privacy Policy" href="<?php echo $rel_path;?>privacy" class="genLink">
 							Privacy Policy
 						</a>
 					</h3>
