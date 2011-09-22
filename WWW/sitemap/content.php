7dcb0256455a3758437a22e0b15385dc30db4336
S0uten <himespike@gmail.com>
2011-09-23 06:45:35 +0800

    Site-Map page: Clean up markup, use global styles

--- WWW/sitemap/content.php
+++ WWW/sitemap/content.php
@@ -1,33 +1,19 @@
-		<h1 class="pageTitle">Site Map</h1>
-		<ul class="styledList">
-			<li>
-				<h3 class="itemTitle">
-					<a title="Home Page" href="<?php echo $rel_path;?>" class="genLink">
-						Home
-					</a>
-				</h3>
-				<ul class="styledList">
+		<article id="sitemap" class="doc">
+			<h1>Site Map</h1>
+			<ul class="logo">
 				<li>
-					<h3 class="itemTitle">
-						<a title="FAQ" href="<?php echo $rel_path;?>faq" class="genLink">
-							FAQ
-						</a>
-					</h3>
+					<h3><a title="Home Page" href=".." class="genLink">Home</a></h3>
+					<ul class="logo">
+						<li>
+							<h3><a title="FAQ" href="../faq" class="genLink">FAQ</a></h3>
+						</li>
+						<li>
+							<h3><a title="Features" href="../features" class="genLink">Features</a></h3>
+						</li>
+						<li>
+							<h3><a title="Privacy Policy" href="../privacy" class="genLink">Privacy Policy</a></h3>
+						</li>
+					</ul>
 				</li>
-				<li>
-					<h3 class="itemTitle">
-						<a title="Features" href="<?php echo $rel_path;?>features" class="genLink">
-							Features
-						</a>
-					</h3>
-				</li>
-				<li>
-					<h3 class="itemTitle">
-						<a title="Privacy Policy" href="<?php echo $rel_path;?>privacy" class="genLink">
-							Privacy Policy
-						</a>
-					</h3>
-				</li>
-				</ul>
-			</li>
-		</ul>
+			</ul>
+		</article>
