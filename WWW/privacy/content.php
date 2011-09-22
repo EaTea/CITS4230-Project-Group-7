75d24b3fcd7f464ff736f9be73859108584238ec
EaTea <ed.r.tay@gmail.com>
2011-09-23 03:54:09 +0800

    Privacy: Added styling to the privacy page. (GH-10)

--- WWW/privacy/content.php
+++ WWW/privacy/content.php
@@ -1,28 +1,32 @@
 		<!-- subject to changes -->
-		<h1>Privacy Policy</h1>
-		<h2>...or Your Info, and How We'll Use It</h2>
-		<p>
-			We actually do care about your privacy! You're making all these importants things you need to do, and there might be something a little whacky and sensitive there. We're going to do our utmost to make sure none of your information is shown to other people without your consent. <br />
-			We've put together a little privacy policy to let you know what you're getting into, and how we'll use your information when you sign up for FMN.
-		</p>
-		<ul>
-			<li>
-				<h3>Account Information</h3>
+		<article id="privacy">
+		<div class="doc">
+			<h1>Privacy Policy</h1>
+			<h2>...or Your Info, and How We'll Use It</h2>
+			<p>
+				We actually do care about your privacy! You're making all these importants things you need to do, and there might be something a little whacky and sensitive there. We're going to do our utmost to make sure none of your information is shown to other people without your consent. <br />
+				We've put together a little privacy policy to let you know what you're getting into, and how we'll use your information when you sign up for FMN.
+			</p>
+		</div>
+		<div id="privacy_list">
+			<section>
+				<h2>Account Information</h2>
 				<p>
 					We ask you for a name, an email and a password. That's it. And those three things aren't going to be spread around - if you find out that your details are on the net, it wasn't us! Scout's honour!
 				</p>
-			</li>
-			<li>
-				<h3>User Communications</h3>
+			</section>
+			<section>
+				<h2>User Communications</h2>
 				<p>
 					If you communicate with someone within FMN, that's going to stay private between you and whoever else you've allowed to view your conversations. Say whatever kinky or weird things you wanna discuss, and have no fear that prying eyes will unwantedly view your, uh, "work". <br />
 					On the other hand, contacting us is as easy as emailing us at taye03@student.uwa.edu.au. We're always looking forward to being told how you've enjoyed/been frustrated by using FMN. We'll take what you said into consideration in order to make FMN even better. Just watch the comments about Edwin's hair...he's a little...sensitive about that...
 				</p>
-			</li>
-			<li>
-				<h3>Tasks</h3>
+			</section>
+			<section>
+				<h2>Tasks</h2>
 				<p>
 					Fear not! Write whatever tasks you want! It's your own business, and we don't read it. Nor do we share it. The weird, wonderful smorgasboard of to-do tasks that you've listed on FMN are perfectly safe on our server.
 				</p>
-			</li>
-		</ul>
+			</section>
+		</div>
+		</article>
