425d14abdf4cdb6e631e328758e7aacf2a21b077
EaTea <ed.r.tay@gmail.com>
2011-09-23 03:04:46 +0800

    References: Styled page and added list styling for these specific elements using CSS classes.

--- WWW/reference/content.php
+++ WWW/reference/content.php
@@ -1,35 +1,41 @@
-		<h1>References</h1>
-		<ul>
+		<h1 class="pageTitle">References</h1>
+		<ul class="styledList">
 			<li>
-				<h3>
-					<a title="Favicon and Logo" href="http://www.leehansen.com/clipart/Holidays/GrandparentsDay/pages/forget-me-not-flower.htm">
+				<h3 class="itemTitle">
+					<a title="Favicon and Logo" href="http://www.leehansen.com/clipart/Holidays/GrandparentsDay/pages/forget-me-not-flower.htm" class="genLink">
 						Favicon and Logo
 					</a>
 				</h3>
 			</li>
 			<li>
-				<h3>
-					<a title="Style and Background" href="http://www.gnome.org/">
+				<h3 class="itemTitle">
+					<a title="Style and Background" href="http://www.gnome.org/" class="genLink">
 						Style and Background
 					</a>
 				</h3>
 			</li>
 			<li>
-				<h3>
+				<h3 class="itemTitle">
 					Slideshow pictures
 				</h3>
-				<ul>
+				<ul class="styledList">
 					<li>
-						<a title="Picture A" href="http://www.whataracquet.com.au/wp-content/uploads/2010/03/to-do-list.jpg">Picture A</a>
-						<a title="Picture B" href="http://browse.deviantart.com/?qh=&section=&q=menma#/d3icrjs">Picture B</a>
-						<a title="Picture C" href="http://www.copyright-free-photos.org.uk/flowers/forget-me-not-08.jpg">Picture C</a>
-						<a title="Picture D" href="http://www.neighborhoodafc.com/calendar.jpg">Picture D</a>
+						<a title="Picture A" href="http://www.whataracquet.com.au/wp-content/uploads/2010/03/to-do-list.jpg" class="genLink">Picture A</a>
+					</li>
+					<li>
+						<a title="Picture B" href="http://browse.deviantart.com/?qh=&section=&q=menma#/d3icrjs" class="genLink">Picture B</a>
+					</li>
+					<li>
+						<a title="Picture C" href="http://www.copyright-free-photos.org.uk/flowers/forget-me-not-08.jpg" class="genLink">Picture C</a>
+					</li>
+					<li>
+						<a title="Picture D" href="http://www.neighborhoodafc.com/calendar.jpg" class="genLink">Picture D</a>
 					</li>
 				</ul>
 			</li>
 			<li>
-				<h3>
-					<a title="Privacy Policy and FAQ" href="http://teuxdeux.com/">
+				<h3 class="itemTitle">
+					<a title="Privacy Policy and FAQ" href="http://teuxdeux.com/" class="genLink">
 						Privacy Policy and FAQ
 					</a>
 				</h3>
