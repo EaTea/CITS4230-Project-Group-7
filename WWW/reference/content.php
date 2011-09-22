ea7b58e29e8d43f487313b1776ce28a4ffd19568
EaTea <ed.r.tay@gmail.com>
2011-09-23 01:54:08 +0800

    References: Added all references but currently unstyled. (GH-19)

---
+++ WWW/reference/content.php
@@ -0,0 +1,37 @@
+		<h1>References</h1>
+		<ul>
+			<li>
+				<h3>
+					<a title="Favicon and Logo" href="http://www.leehansen.com/clipart/Holidays/GrandparentsDay/pages/forget-me-not-flower.htm">
+						Favicon and Logo
+					</a>
+				</h3>
+			</li>
+			<li>
+				<h3>
+					<a title="Style and Background" href="http://www.gnome.org/">
+						Style and Background
+					</a>
+				</h3>
+			</li>
+			<li>
+				<h3>
+					Slideshow pictures
+				</h3>
+				<ul>
+					<li>
+						<a title="Picture A" href="http://www.whataracquet.com.au/wp-content/uploads/2010/03/to-do-list.jpg">Picture A</a>
+						<a title="Picture B" href="http://browse.deviantart.com/?qh=&section=&q=menma#/d3icrjs">Picture B</a>
+						<a title="Picture C" href="http://www.copyright-free-photos.org.uk/flowers/forget-me-not-08.jpg">Picture C</a>
+						<a title="Picture D" href="http://www.neighborhoodafc.com/calendar.jpg">Picture D</a>
+					</li>
+				</ul>
+			</li>
+			<li>
+				<h3>
+					<a title="Privacy Policy and FAQ" href="http://teuxdeux.com/">
+						Privacy Policy and FAQ
+					</a>
+				</h3>
+			</li>
+		</ul>
