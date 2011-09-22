b854d08e7d92ca66ac0b1128a6ddb4c9acb37e94
S0uten <himespike@gmail.com>
2011-09-23 06:06:11 +0800

    Features page: Clean up markup and style

--- WWW/features/content.php
+++ WWW/features/content.php
@@ -1,35 +1,37 @@
 		<!-- Features -->
-		<h1 class="pageTitle">Features</h1>
-		I guess you're wondering what kind of things you'll be able to do on FMN? Plenty! Take a look below at some of FMN's features:
-		<ul class="styledList">
-			<li>
-				<h3 class="itemTitle">Customisable tasks</h3>
-				<p>
-					From their creation to their completion, their description and details, control it all on FMN! The tasks end with you!
-				</p>
-			</li>
-			<li>
-				<h3 class="itemTitle">Categorise your tasks</h3>
-				<p>
-					Add a task to a list (or lists!) of things-to-do and then share it with your friends and colleagues, so you can get everyone organised!
-				</p>
-			</li>
-			<li>
-				<h3 class="itemTitle">List permissions</h3>
-				<p>
-				 Need to make a list that people can view, but they can't change? It's easy with FMN's List Permissions system!
-			</li>
-			<li>
-				<h3 class="itemTitle">Automated Email Reminders</h3>
-				<p>
-					Yep, we too are looking forward to flooding your emails with more spam (but we really hope that you use our website instead).
-				</p>
-			</li>
-			<li>
-				<h3 class="itemTitle">Offline Storage</h3>
-				<p>
-					Can't actually get to FMN at a point in time? Never fear! You can use our handy Local Synch feature to get a copy of your to-do list and store it on your computer.
-				</p>
-			</li>
-		</ul>
-		If you've got some questions, you should go to our <a href="../faq">FAQ</a> to learn more.
+		<article id="features" class="doc">
+			<h1>Features</h1>
+			I guess you're wondering what kind of things you'll be able to do on FMN? Plenty! Take a look below at some of FMN's features:
+			<ul class="styledList">
+				<li>
+					<h2>Customisable tasks</h2>
+					<p>
+						From their creation to their completion, their description and details, control it all on FMN! The tasks end with you!
+					</p>
+				</li>
+				<li>
+					<h2>Categorise your tasks</h2>
+					<p>
+						Add a task to a list (or lists!) of things-to-do and then share it with your friends and colleagues, so you can get everyone organised!
+					</p>
+				</li>
+				<li>
+					<h2>List permissions</h2>
+					<p>
+					 Need to make a list that people can view, but they can't change? It's easy with FMN's List Permissions system!
+				</li>
+				<li>
+					<h2>Automated Email Reminders</h2>
+					<p>
+						Yep, we too are looking forward to flooding your emails with more spam (but we really hope that you use our website instead).
+					</p>
+				</li>
+				<li>
+					<h2>Offline Storage</h2>
+					<p>
+						Can't actually get to FMN at a point in time? Never fear! You can use our handy Local Synch feature to get a copy of your to-do list and store it on your computer.
+					</p>
+				</li>
+			</ul>
+			If you've got some questions, you should go to our <a href="../faq">FAQ</a> to learn more.
+		</article>
