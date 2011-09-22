fb25ac9c8f3730a1a549a062548bf4dba1ff5e1e
EaTea <ed.r.tay@gmail.com>
2011-09-23 03:53:35 +0800

    Features: Added styling to the features page. (GH-10)

--- WWW/features/content.php
+++ WWW/features/content.php
@@ -1,21 +1,35 @@
 		<!-- Features -->
-		<h1>Features</h1>
+		<h1 class="pageTitle">Features</h1>
 		I guess you're wondering what kind of things you'll be able to do on FMN? Plenty! Take a look below at some of FMN's features:
-		<ul>
+		<ul class="styledList">
 			<li>
-				Customisable tasks, from their creation to their completion and details
+				<h3 class="itemTitle">Customisable tasks</h3>
+				<p>
+					From their creation to their completion, their description and details, control it all on FMN! The tasks end with you!
+				</p>
 			</li>
 			<li>
-				Categorise your tasks: add a task to a list (or lists!) of things-to-do and then share it with your friends and colleagues
+				<h3 class="itemTitle">Categorise your tasks</h3>
+				<p>
+					Add a task to a list (or lists!) of things-to-do and then share it with your friends and colleagues, so you can get everyone organised!
+				</p>
 			</li>
 			<li>
-				List permissions: need to make a list that people can view, but they can't change? It's easy with FMN's List Permissions system
+				<h3 class="itemTitle">List permissions</h3>
+				<p>
+				 Need to make a list that people can view, but they can't change? It's easy with FMN's List Permissions system!
 			</li>
 			<li>
-				Automated Email Reminders: yep, we too are looking forward to flooding your emails with more spam (but we really hope that you use our website instead)
+				<h3 class="itemTitle">Automated Email Reminders</h3>
+				<p>
+					Yep, we too are looking forward to flooding your emails with more spam (but we really hope that you use our website instead).
+				</p>
 			</li>
 			<li>
-				Offline Storage: Can't actually get to FMN at a point in time? Never fear! You can use our handy Local Synch feature to get a copy of your to-do list and store it on your computer
+				<h3 class="itemTitle">Offline Storage</h3>
+				<p>
+					Can't actually get to FMN at a point in time? Never fear! You can use our handy Local Synch feature to get a copy of your to-do list and store it on your computer.
+				</p>
 			</li>
 		</ul>
 		If you've got some questions, you should go to our <a href="../faq">FAQ</a> to learn more.
