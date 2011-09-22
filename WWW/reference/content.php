52b7c0d71324be6aacbc81b0e7909fb407195d8b
S0uten <himespike@gmail.com>
2011-09-23 06:44:58 +0800

    Reference page: Clean up markup, use global styles

--- WWW/reference/content.php
+++ WWW/reference/content.php
@@ -1,43 +1,31 @@
-		<h1 class="pageTitle">References</h1>
-		<ul class="styledList">
-			<li>
-				<h3 class="itemTitle">
-					<a title="Favicon and Logo" href="http://www.leehansen.com/clipart/Holidays/GrandparentsDay/pages/forget-me-not-flower.htm" class="genLink">
-						Favicon and Logo
-					</a>
-				</h3>
-			</li>
-			<li>
-				<h3 class="itemTitle">
-					<a title="Style and Background" href="http://www.gnome.org/" class="genLink">
-						Style and Background
-					</a>
-				</h3>
-			</li>
-			<li>
-				<h3 class="itemTitle">
-					Slideshow pictures
-				</h3>
-				<ul class="styledList">
-					<li>
-						<a title="Picture A" href="http://www.whataracquet.com.au/wp-content/uploads/2010/03/to-do-list.jpg" class="genLink">Picture A</a>
-					</li>
-					<li>
-						<a title="Picture B" href="http://browse.deviantart.com/?qh=&amp;section=&amp;q=menma#/d3icrjs" class="genLink">Picture B</a>
-					</li>
-					<li>
-						<a title="Picture C" href="http://www.copyright-free-photos.org.uk/flowers/forget-me-not-08.jpg" class="genLink">Picture C</a>
-					</li>
-					<li>
-						<a title="Picture D" href="http://www.neighborhoodafc.com/calendar.jpg" class="genLink">Picture D</a>
-					</li>
-				</ul>
-			</li>
-			<li>
-				<h3 class="itemTitle">
-					<a title="Privacy Policy and FAQ" href="http://teuxdeux.com/" class="genLink">
-						Privacy Policy and FAQ
-					</a>
-				</h3>
-			</li>
-		</ul>
+		<article id="reference" class="doc">
+			<h1>References</h1>
+			<ul class="logo">
+				<li>
+					<h3><a title="Favicon and Logo" href="http://www.leehansen.com/clipart/Holidays/GrandparentsDay/pages/forget-me-not-flower.htm" class="genLink">Favicon and Logo</a></h3>
+				</li>
+				<li>
+					<h3><a title="Style and Background" href="http://www.gnome.org/" class="genLink">Style and Background</a></h3>
+				</li>
+				<li>
+					<h3>Slideshow pictures</h3>
+					<ul class="logo">
+						<li>
+							<a title="Picture A" href="http://www.whataracquet.com.au/wp-content/uploads/2010/03/to-do-list.jpg" class="genLink">Picture A</a>
+						</li>
+						<li>
+							<a title="Picture B" href="http://browse.deviantart.com/?qh=&amp;section=&amp;q=menma#/d3icrjs" class="genLink">Picture B</a>
+						</li>
+						<li>
+							<a title="Picture C" href="http://www.copyright-free-photos.org.uk/flowers/forget-me-not-08.jpg" class="genLink">Picture C</a>
+						</li>
+						<li>
+							<a title="Picture D" href="http://www.neighborhoodafc.com/calendar.jpg" class="genLink">Picture D</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<h3><a title="Privacy Policy and FAQ" href="http://teuxdeux.com/" class="genLink">Privacy Policy and FAQ</a></h3>
+				</li>
+			</ul>
+		</article>
