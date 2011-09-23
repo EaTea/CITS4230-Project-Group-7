255fe5025c909b12dbd193b4cb5f6a01f8c90f7d
S0uten <himespike@gmail.com>
2011-09-23 08:09:27 +0800

    Reference page: Add more detail

--- WWW/reference/content.php
+++ WWW/reference/content.php
@@ -2,30 +2,45 @@
 			<h1>References</h1>
 			<ul class="logo">
 				<li>
-					<h3><a title="Favicon and Logo" href="http://www.leehansen.com/clipart/Holidays/GrandparentsDay/pages/forget-me-not-flower.htm" class="genLink">Favicon and Logo</a></h3>
+					<h3>Favicon and Logo</h3>
+					<ul class="logo">
+						<li>
+							<a title="Favicon and Logo" href="http://www.leehansen.com/clipart/Holidays/GrandparentsDay/pages/forget-me-not-flower.htm" class="genLink">Grandparents Day Clip Art: Forget Me Not Flowers</a>
+						</li>
+					</ul>
 				</li>
 				<li>
-					<h3><a title="Style and Background" href="http://www.gnome.org/" class="genLink">Style and Background</a></h3>
+					<h3>Style and Background</h3>
+					<ul class="logo">
+						<li>
+							<a title="Style and Background" href="http://www.gnome.org/" class="genLink">GNOME</a>
+						</li>
+					</ul>
 				</li>
 				<li>
 					<h3>Slideshow pictures</h3>
 					<ul class="logo">
 						<li>
-							<a title="Picture A" href="http://www.whataracquet.com.au/wp-content/uploads/2010/03/to-do-list.jpg" class="genLink">Picture A</a>
+							<a title="Picture A" href="http://www.whataracquet.com.au/wp-content/uploads/2010/03/to-do-list.jpg" class="genLink">Picture A: http://www.whataracquet.com.au/wp-content/uploads/2010/03/to-do-list.jpg</a>
 						</li>
 						<li>
-							<a title="Picture B" href="http://browse.deviantart.com/?qh=&amp;section=&amp;q=menma#/d3icrjs" class="genLink">Picture B</a>
+							<a title="Picture B" href="http://browse.deviantart.com/?qh=&amp;section=&amp;q=menma#/d3icrjs" class="genLink">Picture B: http://browse.deviantart.com/?qh=&amp;section=&amp;q=menma#/d3icrjs</a>
 						</li>
 						<li>
-							<a title="Picture C" href="http://www.copyright-free-photos.org.uk/flowers/forget-me-not-08.jpg" class="genLink">Picture C</a>
+							<a title="Picture C" href="http://www.copyright-free-photos.org.uk/flowers/forget-me-not-08.jpg" class="genLink">Picture C: http://www.copyright-free-photos.org.uk/flowers/forget-me-not-08.jpg</a>
 						</li>
 						<li>
-							<a title="Picture D" href="http://www.neighborhoodafc.com/calendar.jpg" class="genLink">Picture D</a>
+							<a title="Picture D" href="http://www.neighborhoodafc.com/calendar.jpg" class="genLink">Picture D: http://www.neighborhoodafc.com/calendar.jpg</a>
 						</li>
 					</ul>
 				</li>
 				<li>
-					<h3><a title="Privacy Policy and FAQ" href="http://teuxdeux.com/" class="genLink">Privacy Policy and FAQ</a></h3>
+					<h3>Privacy Policy and FAQ</h3>
+					<ul class="logo">
+						<li>
+							<a title="Privacy Policy and FAQ" href="http://teuxdeux.com/" class="genLink">What deux yeux have teux deux teuxday?</a>
+						</li>
+					</ul>
 				</li>
 			</ul>
 		</article>
