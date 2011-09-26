e884a7a150339ca7b7fd601c68a33d16b1e6f6b5
S0uten <himespike@gmail.com>
2011-09-26 15:50:06 +0800

    Rails: Add initial template files

---
+++ WWW/todo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = '309a81fd0a3e283571fdc00f79f8ec41cc2b26eb0ca4839b2b9de77f723698fa88a65fecea9338f8ef8b47eaa6e4e6cd535db11e6de76676a3a28a01ad9d8a58'
