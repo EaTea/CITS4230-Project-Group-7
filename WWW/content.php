db21880cc06e13ed901d010b496a6ebc7105d49e
S0uten <himespike@gmail.com>
2011-10-05 18:52:00 +0800

    Slideshow: Move caption and adjust styles (Close GH-20)

--- WWW/content.php
+++ WWW/content.php
@@ -2,15 +2,15 @@
 			Shared to-do lists let you manage and share schedules with your friends!
 			<figure id="slideshow">
 				<div class="img_container">
-					<img src="images/fmn-A.jpg" alt="A" title="FMN: the exciting shared To-Do List website! Read more <a href='./features'>here</a>!"/>
+					<img src="images/fmn-A.jpg" alt="A" title="FMN: the exciting shared To-Do List website!<br />Read more <a href='./features'>here</a>!"/>
 					<img src="images/fmn-B.jpg" alt="B" title="Are you feeling a little confused?<br />See our <a href='./faq'>FAQ</a> and we'll point you in the right direction!"/>
 					<img src="images/fmn-C.jpg" alt="C" title="We do our utmost to ensure that your data is safely secured.<br />Read about our <a href='./privacy'>privacy policy</a>!"/>
 					<img src="images/fmn-D.jpg" alt="D" title="Wanna join? It's easy!<br />Just fill in the form to your right and you're set!"/>
 				</div>
+				<figcaption></figcaption>
 				<div class="selector">
 					<ul></ul>
 				</div>
-				<figcaption></figcaption>
 			</figure>
 			<a href="features">Learn more...</a>
 		</article>
