4b34976ac29ce4b4bae977f8e58f24639c99cccc
sontobgay <sontobdrukpa@gmail.com>
2011-10-06 11:32:31 +0800

    Sign-in: skeletal layout of sign-in page

---
+++ WWW/protected/index.php
@@ -0,0 +1,73 @@
+<!doctype html>  
+<head>
+<meta charset="UTF-8">
+<title>The Shared T0-Do Signed In Page - Home</title>
+</head>
+<body>
+    <!--start container-->
+    <div id="container">
+        <header>
+            <a href="#" id="logo"><img src="images/xyz.png" width="221" height="130" alt="logo"/></a>    
+            <nav>
+            <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Menus</a></li>
+            <li><a href="#">User: sontobgay logged in</a></li>
+            <li><a href="#">Logout</a></li>
+            </ul>
+            </nav>
+        </header>
+   
+       <!--start holder-->
+       <div class="holder_content">
+           <!--start column one-->
+           <section class="todo-menu">
+            <h3>Menu: To-Do</h3>
+            <p>
+               <nav>
+                <ul>
+                <li><a href="#">Manage List: View</a></li>
+                <li><a href="#">Manage List: Add</a></li>
+                <li><a href="#">Manage List: Edit</a></li>
+                <li><a href="#">Manage List: Delete</a></li>
+                <li><a href="#">Manage Task: View</a></li>
+                <li><a href="#">Manage Task: Add</a></li>
+                <li><a href="#">Manage Task: Edit</a></li>
+                <li><a href="#">Manage Task: Delete</a></li>
+                </ul>
+                </nav>  
+            </p>
+           </section>
+
+           <section class="to-do-calender">
+               <h3>Select Your To-Do Date</h3>
+               <p>
+               </p>
+           </section>
+
+           <section class="upcoming-task">
+           <h3>Upcoming Task </h3>
+           <p>
+               <ul>
+                <li><a href="#">task1</a></li>
+                <li><a href="#">task2</a></li>
+                <li><a href="#">task3</a></li>
+                <li><a href="#">task4</a></li>
+                </ul>
+           </p>
+           </section>
+       </div>
+       <!--end holder-->
+</div>
+<!--end container-->
+   
+       <!--start footer-->
+       <footer>
+       <div class="container">
+       <div id="footer-right"> © CITS4230G Group </div>
+       <div id="footer-left">Valid HTML5</div> 
+       </div>
+       </footer>
+   <!--end footer-->
+</body>
+</html
