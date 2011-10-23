42944471a7d3093e2d4c0c6e099c00afe382d682
EaTea <ed.r.tay@gmail.com>
2011-10-23 23:45:48 +0800

    Model: in user, add email validation using a custom validator.

--- todo/app/models/user.rb
+++ todo/app/models/user.rb
@@ -1,7 +1,20 @@
+# Code taken from Ruby-on-Rails guide
+# http://guides.rubyonrails.org/active_record_validations_callbacks.html 
+
+class EmailValidator < ActiveModel::Validator
+  def validate(record)
+		@value = record.email
+    unless @value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
 class User < ActiveRecord::Base
+	include ActiveModel::Validations
 	validates :username, :presence=>true, :length=>{:maximum=>32}, :uniqueness=>true
 	validates :password, :presence=>true, :length=>{:maximum=>128}
 	validates :salt, :presence=>true, :length=>{:is=>16}
 	validates :email, :presence => true, :length=>{:maximum=>64}, :uniqueness=>true
-
+	validates_with EmailValidator
 end
