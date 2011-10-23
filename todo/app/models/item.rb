97913fbaa3845d986131af06ea7c0eb539f89cbc
EaTea <ed.r.tay@gmail.com>
2011-10-23 23:21:31 +0800

    Models: Add initial user validations.

--- todo/app/models/item.rb
+++ todo/app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ActiveRecord::Base
+	validates :username, :presence=>true, :length=>{:maximum=>32}, :uniqueness=>true
+	validates :password, :presence=>true, :length=>{:maximum=>128}
+	validates :salt, :presence=>true, :length=>{:maximum=>16}
+	validates :email, :presence => true, :length=>{:maximum=>64}, :uniqueness=>true
 end
--- todo/app/models/permission.rb
+++ todo/app/models/permission.rb
@@ -1,2 +1,10 @@
 class Permission < ActiveRecord::Base
+	validates :add, :existence => true
+	validates :edit, :existence => true
+	validates :delete, :existence => true
+	validates :boolean, :existence => true
+	validates :user_id, :existence => true
+	validates :list_id, :existence => true
+	belongs_to user
+	belongs_to list
 end
