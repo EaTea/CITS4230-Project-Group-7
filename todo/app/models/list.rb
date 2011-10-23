87b0166d8c088b60867f42c6558b8a2428538fe9
EaTea <ed.r.tay@gmail.com>
2011-10-23 23:53:03 +0800

    Models: Add referential integrity validation for list model.

--- todo/app/models/list.rb
+++ todo/app/models/list.rb
@@ -1,3 +1,7 @@
 class List < ActiveRecord::Base
 	validates :name, :presence => true, :length => {:maximum => 64}
+#Referential Integrity validation
+	has_many :permissions
+	has_many :users, :through => :permissions
+	has_many :tasks
 end
