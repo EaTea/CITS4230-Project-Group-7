2cc9b8ca4b2f2ab4b47512f07805d832296394a0
EaTea <ed.r.tay@gmail.com>
2011-10-26 02:36:10 +0800

    Models: Add referential integrity validation to items.

--- todo/app/models/item.rb
+++ todo/app/models/item.rb
@@ -1,11 +1,11 @@
 class Item < ActiveRecord::Base
-	validates :name, :presence=>true, :length=>{:maximum => 64}
-	validates :description, :length =>{:maximum => 128}
-	validates :due_date, :presence=>true
+	validates :name, :presence => true, :length=>{:maximum => 64}
+	validates :description, :length  => {:maximum => 128}
+	validates :date, :presence => true
 	#I think the date's automatically in the right format so I'm not
 	#doing extra validation on it
-	validates :completed, :presence=>true
-	validates :list_id, :presence=>true
+	validates :list_id, :presence => true
 #Referential integrity validation
 	belongs_to :list
+	validates :list, :presence => true
 end
