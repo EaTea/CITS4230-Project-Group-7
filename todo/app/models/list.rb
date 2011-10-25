fa789b1d5964db9cf0621377fced1fb9914d3dde
EaTea <ed.r.tay@gmail.com>
2011-10-26 01:39:54 +0800

    Models: Add depedent destruction to list-permission relation.

--- todo/app/models/list.rb
+++ todo/app/models/list.rb
@@ -1,7 +1,7 @@
 class List < ActiveRecord::Base
 	validates :name, :presence => true, :length => {:maximum => 64}
 #Referential Integrity validation
-	has_many :permissions
+	has_many :permissions, :dependent => :destroy
 	has_many :users, :through => :permissions
 	has_many :items, :dependent => :destroy
 	validates_associated :items
