a50ee0ce7d0fef090509f2a0b0a38e5a0157662e
EaTea <ed.r.tay@gmail.com>
2011-10-25 15:09:17 +0800

    Models: Add uniqueness check for list and user ids in permission.

--- todo/app/models/permission.rb
+++ todo/app/models/permission.rb
@@ -1,16 +1,12 @@
-#require 'rubygems'
-#require 'composite_primary_keys'
-
 class Permission < ActiveRecord::Base
-	validates :add, :presence => true
-	validates :edit, :presence => true
-	validates :delete, :presence => true
-	validates :own, :presence => true
-	validates :user_id, :presence => true
-	validates :list_id, :presence => true
+#validates :add, :presence => true
+#validates :edit, :presence => true
+#	validates :delete, :presence => true
+#	validates :own, :presence => true
+	validates :user_id, :presence => true, :uniqueness => { :scope => :list_id}
+	validates :list_id, :presence => true, :uniqueness => { :scope => :user_id}
 	belongs_to :user
 	belongs_to :list
-	validates_associated :user
-	validates_associated :list
-#	set_primary_keys :list_id, :user_id
+#validates_associated :user
+#	validates_associated :list
 end
