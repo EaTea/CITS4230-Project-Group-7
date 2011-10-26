831039c223b520e54bf32dfced91922745c2c35c
EaTea <ed.r.tay@gmail.com>
2011-10-26 13:11:31 +0800

    Models: Properly encode list<->user relation validation and deletion.

--- todo/app/models/user.rb
+++ todo/app/models/user.rb
@@ -14,7 +14,6 @@ end
 
 class User < ActiveRecord::Base
 	include ActiveModel::Validations
-#attr_accessor :id
 	attr_accessible :username, :email, :password, :password_confirmation, :email_confirmation
 	validates :username, :presence=>true, :length=>{:maximum=>32}, :uniqueness=>true
 	validates :password, :presence=>true, :length=>{:maximum=>128}, :confirmation => true
@@ -25,33 +24,48 @@ class User < ActiveRecord::Base
 	validates :email_confirmation, :presence => true, :length=>{:maximum=>64}, :confirmation => true
 	validates_with EmailValidator, :if => "email?"
 
-	#automatically generate salt
-	before_validation(:on => :create) do
-		self.salt = make_salt if new_record?
-	end
+	#automatically generate salt and encrypts password
+	before_save :encrypt_password
+	before_destroy :is_only_owner
 
 #Validations for referential integrity follow
 	has_many :permissions, :dependent => :destroy
 	has_many :lists, :through => :permissions
 
-	before_destroy :is_only_owner
+
+	def authenticate?(sub_password)
+		password == encrypt(sub_password)
+	end
 
 	private 
+		def encrypt_password
+			self.salt = make_salt if new_record? 
+			self.password = encrypt(password)
+		end
 		def make_salt
   	 	secure_hash("#{Time.now.utc}--#{password}")
 			#secure_hash to get a hash of current time
+  	end
+		def encrypt(string)
+			temp_string = string
+	   	for i in 1..3 do 
+				temp_string = secure_hash("#{salt}--#{temp_string}") 
+			end
+			temp_string
   	end
 		def secure_hash(string)
   	 	Digest::SHA2.hexdigest(string)
   	end
 		def is_only_owner
-			sql_find_lists = "SELECT l.* FROM lists l, permissions p WHERE l.id = p.list_id AND p.user_id = " + id.to_s
-			#not neat but incorrect behaviour when using other notation [string, args]
+			sql_find_lists = "SELECT l.* FROM lists l, permissions p WHERE p.own = 't' AND l.id = p.list_id AND p.user_id = " + id.to_s
+			#not neat but I got incorrect behaviour when using other notation [string, args]
+			puts 'entering this method!'
 			del_candidates = List.find_by_sql sql_find_lists
 			del_candidates.each do |l|
-				perms = Permission.find_by_sql ["SELECT * FROM permissions WHERE user_id = ? AND list_id = ? AND own = 't'", id, @l.id]
+				perms = Permission.find_by_sql ["SELECT * FROM permissions WHERE list_id = ? AND own = 't'", l.id]
 				if perms.size == 1
-					@l.destroy
+					puts l.id
+					l.destroy
 				end
 			end
 			#incorrect behaviour when splitting into lines, tried using semi-colon
