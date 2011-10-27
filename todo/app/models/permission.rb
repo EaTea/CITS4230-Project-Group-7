9e37413035c7dd72d108b5e48019a2fb3007187a
EaTea <ed.r.tay@gmail.com>
2011-10-28 00:58:29 +0800

    Models: Add validation so that no list is orphaned and unowned.
    This validation was overlooked in the initial models. I'm correcting
    this now so that if an owner decides to mark themselves as no longer
    an owner of a list or deletes their permissions then the system will
    error.

--- todo/app/models/permission.rb
+++ todo/app/models/permission.rb
@@ -1,10 +1,21 @@
 class Permission < ActiveRecord::Base
+	
 	validates :user_id, :presence => true, :uniqueness => { :scope => :list_id}
 	validates :list_id, :presence => true, :uniqueness => { :scope => :user_id}
 	validates :user, :presence => { :message => "should be an existing user" }
 	validates :list, :presence => { :message => "should be an existing list" }
+
+	validate :user_is_not_only_owner, :on => :update, :one => :delete, :if => "own_changed? and own == false"
+
 	belongs_to :user
 	belongs_to :list
-#validates_associated :user
-#	validates_associated :list
+	
+	private 
+
+		def user_is_not_only_owner
+			perms = Permission.find_by_sql ["SELECT * FROM permissions WHERE list_id = ? AND own = 't'", list_id.to_s]
+			puts list_id.to_s
+			errors.add(:own, " -- You are the last owner of the list!" + 
+					" Please assign someone else ownership before you change your permissions.") if perms.size == 1
+		end
 end
