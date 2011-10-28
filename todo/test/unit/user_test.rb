1c64df140def9797f1518e577b0eba495dd5b492
sontobgay <sontobdrukpa@gmail.com>
2011-10-29 01:43:20 +0800

    Unit Test: test password length in User Model

--- todo/test/unit/user_test.rb
+++ todo/test/unit/user_test.rb
@@ -8,4 +8,15 @@ class UserTest < ActiveSupport::TestCase
 		assert user.errors[:password].any?
 		assert user.errors[:email].any?
   end
+  
+  test "password length check" do
+    user = User.new
+    user.password < 6
+    assert user.invalid? 
+    assert_equal "Must be 6 or more characters",
+      user.error[:password].join('; ')
+    
+    user.password >= 6
+    assert user.valid?
+  end
 end
