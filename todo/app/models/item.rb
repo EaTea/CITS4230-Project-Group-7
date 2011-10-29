0036a5d46b2497050298b793728d2c1fee99b870
EaTea <ed.r.tay@gmail.com>
2011-10-30 05:29:01 +0800

    Models: Ensure that a new todo-task will only ever exist in the future.
    I can't test this due to the bug I have with page redirects.

--- todo/app/models/item.rb
+++ todo/app/models/item.rb
@@ -2,10 +2,18 @@ class Item < ActiveRecord::Base
 	validates :name, :presence => true, :length=>{:maximum => 64}
 	validates :description, :length  => {:maximum => 128}
 	validates :due_date, :presence => true
+	validates :is_later_than_today
 	#I think the date's automatically in the right format so I'm not
 	#doing extra validation on it
 	validates :list_id, :presence => true
 #Referential integrity validation
 	belongs_to :list
 	validates :list, :presence => { :message => "must be a valid list" }
+
+	def is_later_than_today(record)
+		@value = record.due_date
+		unless @value >= DateTime.now
+			record.errors[:due_date] << ("must be at a future date")
+		end
+	end
 end
