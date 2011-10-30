281443cb3e943bfa3b1b171fdd7e1f00e167eac9
EaTea <ed.r.tay@gmail.com>
2011-10-30 22:29:20 +0800

    Views: Correct syntax for parameters in redirect (GH-41)

--- todo/app/controllers/items_controller.rb
+++ todo/app/controllers/items_controller.rb
@@ -81,7 +81,7 @@ class ItemsController < ApplicationController
 		@item.destroy
 
 		respond_to do |format|
-			format.html	{ redirect_to(items_url) }
+			format.html	{ redirect_to :action => 'index', :list_id => session[:list_id]  }
 			format.xml	{ head :ok }
 		end
 	end
