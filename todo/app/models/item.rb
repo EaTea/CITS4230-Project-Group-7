43b5b82ddd0a780d9711e7c1fe5d6bf3262d2bf5
EaTea <ed.r.tay@gmail.com>
2011-10-30 21:59:12 +0800

    Models: Fix validation issue with item; wrong syntax for custom method

--- todo/app/models/item.rb
+++ todo/app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ActiveRecord::Base
 	validates :name, :presence => true, :length=>{:maximum => 64}
 	validates :description, :length  => {:maximum => 128}
 	validates :due_date, :presence => true
-	validates :is_later_than_today
+	validate :is_later_than_today
 	#I think the date's automatically in the right format so I'm not
 	#doing extra validation on it
 	validates :list_id, :presence => true
@@ -10,10 +10,9 @@ class Item < ActiveRecord::Base
 	belongs_to :list
 	validates :list, :presence => { :message => "must be a valid list" }
 
-	def is_later_than_today(record)
-		@value = record.due_date
-		unless @value >= DateTime.now
-			record.errors[:due_date] << ("must be at a future date")
+	def is_later_than_today
+		unless due_date >= Date.today
+			errors[:due_date] << ("must be at a future date")
 		end
 	end
 end
