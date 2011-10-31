d9ad8a57dd0503bab36ec3e128ad33885d168699
S0uten <himespike@gmail.com>
2011-10-31 23:58:00 +0800

    Rails: General consistency cleanup

--- todo/app/models/permission.rb
+++ todo/app/models/permission.rb
@@ -1,5 +1,4 @@
 class Permission < ActiveRecord::Base
-	
 	validates :user_id, :presence => true, :uniqueness => { :scope => :list_id}
 	validates :list_id, :presence => true, :uniqueness => { :scope => :user_id}
 	validates :user, :presence => { :message => "should be an existing user" }
