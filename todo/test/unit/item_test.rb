9ce45477351f88a5e7d2dafee2870ff4db6b6c9a
EaTea <ed.r.tay@gmail.com>
2011-11-03 00:33:40 +0800

    Tests: Add some stuff to the unit tests to make the more complete

--- todo/test/unit/item_test.rb
+++ todo/test/unit/item_test.rb
@@ -6,7 +6,7 @@ class ItemTest < ActiveSupport::TestCase
 		item = Item.new
 		assert item.invalid?
 		assert item.errors[:name].any?
-		assert item.errors[:description].any? == false
+		assert item.errors[:description].any? == false #description could be blank
 		assert item.errors[:due_date].any?
 		assert item.errors[:list_id].any?
 		assert true
@@ -14,13 +14,44 @@ class ItemTest < ActiveSupport::TestCase
   
   test "items of todo list" do
     #create a new List object using the name field from first record in the text fixtures/lists.yml 
-    todo_list_items1 = Item.new :name => items(:one).name,
+    todo_list_items1 = Item.new (:name => items(:one).name,
                                 :description => items(:one).description,
                                 :due_date => items(:one).due_date,
-                                :list_id => items(:one).list_id
+                                :list_id => items(:one).list_id,
+																:id => 10)
 
-    #This assertion test that saving the book object is successful: Test the ability to create DB record
-    assert todo_list_items1.save
+		#assert that a nonexistent list will cause a failure
+    assert !todo_list_items1.valid?
+		
+		List.new(:name => "temp", :id => items(:one).list_id).save
+
+		#assert that an item with a due date in the past cannot be considered valid (must always go forward)
+    assert !todo_list_items1.valid?
+		assert todo_list_items1.errors[:due_date].any?
+
+		todo_list_items1.due_date = Date.today + 10
+
+    assert todo_list_items1.valid?
+
+		assert todo_list_items1.save
+
+		name_too_long = Item.new(		:name => items(:one).name + "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+                                :description => items(:one).description,
+                                :due_date => Date.today + 10,
+                                :list_id => items(:one).list_id,
+																:id => 10)
+
+    assert !name_too_long.valid?
+    assert name_too_long.errors[:name].any?
+
+		description_too_long = Item.new(		:name => items(:one).name,
+                               					:description => items(:one).description + "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+                                				:due_date => Date.today + 10,
+                                				:list_id => items(:one).list_id,
+																				:id => 10)
+
+    assert !description_too_long.valid?
+    assert description_too_long.errors[:description].any?
 
     todo_list_items1_copy = Item.find(todo_list_items1.id)
 
