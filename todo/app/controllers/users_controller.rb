4740843071b45876a64b4ec607d3049d74aa7ad4
S0uten <himespike@gmail.com>
2011-11-03 16:39:38 +0800

    User controller: Let admin update users

--- todo/app/controllers/users_controller.rb
+++ todo/app/controllers/users_controller.rb
@@ -62,8 +62,7 @@ class UsersController < ApplicationController
 	# PUT /users/1
 	# PUT /users/1.xml
 	def update
-#if !is_admin?(session[:user_id]) and 
-		if session[:user_id].to_i.to_s != params[:id].to_i.to_s
+		if !is_admin?(session[:user_id]) and session[:user_id].to_i.to_s != params[:id].to_i.to_s
 			@user = User.find(session[:user_id])
 			respond_to do |format|
 				format.html	{ render :action => "show" }
